8417ddb1867fa97ca490ca8bb5121b1d13e0107f
pnocera <pnocera@gmail.com>
2019-11-29 16:46:06 +0100

    second commit

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,107 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as fs from "fs";
+
+
+
+
+export function config() {
+  return vscode.workspace.getConfiguration("translococtx");
+}
+
+export function GetTranlateFiles() {
+  let rootpath = config().get<string>("transloco.jsonrootfolder");
+  if (rootpath && rootpath !== "") {
+    return Promise.resolve(fs.readdirSync(rootpath));
+  }
+  let pr = vscode.workspace.findFiles("*.json").then(obj => {
+    return obj.map(u => {
+      return u.fsPath;
+    });
+  });
+  return Promise.resolve(pr);
+}
+
+export function PickKey(file: string) {
+  if (config().get("transloco.selectfirstlevelobjectkeys")) {
+    let mainobj = JSON.parse(fs.readFileSync(file).toString());
+    if (mainobj) {
+      let keys = Object.keys(mainobj);
+      vscode.window
+        .showQuickPick(keys, { canPickMany: false })
+        .then(picked2 => {
+          return Promise.resolve(picked2);
+        });
+    } else {
+      return Promise.resolve("");
+    }
+  }
+
+  return Promise.resolve("");
+}
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "translococtx" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "translococtx" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "extension.transLocate",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      const editor = vscode.window.activeTextEditor;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.transLocate', () => {
-		// The code you place here will be executed every time your command is executed
+      if (editor) {
+        const selection = editor.selection;
+        const text = editor.document.getText(editor.selection);
+        if (text && text !== "") {
+          GetTranlateFiles().then(ffiles => {
+            vscode.window
+              .showQuickPick(ffiles, { canPickMany: true })
+              .then(picked => {
+                if (picked) {
+                  PickKey(picked[0]).then(key => {
+                    let txt = text.split(" ").join("_");
+                    const newtext = `{{t('${txt}')}}`;
+                    editor.edit(builder => builder.replace(selection, newtext));
+                    picked.map(o => {
+                      fs.readFile(o, (err, data) => {
+                        if (!err) {
+                          let json = JSON.parse(data.toString());
+                          if (key !== undefined && key !== null && key !== "") {
+                            json[key][txt] = `${text}`;
+                          } else {
+                            json[txt] = `${text}`;
+                          }
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+                          fs.writeFile(o, JSON.stringify(json), err1 => {
+                            if (err1) {
+                              vscode.window.showInformationMessage(
+                                err1.message
+                              );
+                            }
+                          });
+                        } else {
+                          vscode.window.showInformationMessage(err.message);
+                        }
+                      });
+                    });
+                  });
+                }
+              });
+          });
+        }
+      }
+    }
+  );
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
